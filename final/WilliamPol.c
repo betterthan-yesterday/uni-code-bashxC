5fb2b0866b00f1605e48861df05e8386f191cd25
William Pol <103951515+betterthan-yesterday@users.noreply.github.com>
2023-11-13 20:24:00 -0500

    Completed strength evaluator

--- final/WilliamPol.c
+++ final/WilliamPol.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int hasLowercase(char str[]) {
-    
+    for (int i = 0; i < strlen(str); i++) {
+        if (islower(str[i]) && isalpha(str[i])) return 1;
+    }
+    return 0;
 }
 
 int hasUppercase(char str[]) {
-    
+    for (int i = 0; i < strlen(str); i++) {
+        if (isupper(str[i]) && isalpha(str[i])) return 1;
+    }
+    return 0;
 }
 
 int hasDigit(char str[]) {
-    
+    for (int i = 0; i < strlen(str); i++) {
+        if (isdigit(str[i])) return 1;
+    }
+    return 0;
 }
 
 int hasSpecialChar(char str[]) {
-    
+    for (int i = 0; i < strlen(str); i++) {
+        if (!isdigit(str[i]) && !isalpha(str[i])) return 1;
+    }
+    return 0;
 }
 
 int evaluateStrength(char str[]) {
-    return hasLowercase(str) + hasUppercase(str) + hasDigit(str) + hasSpecialChar(str) + strlen(str) > 11;
+    if (strlen(str) > 11) return hasLowercase(str) + hasUppercase(str) + hasDigit(str) + hasSpecialChar(str) + 1;
+    return hasLowercase(str) + hasUppercase(str) + hasDigit(str) + hasSpecialChar(str);
 }
 
 int main () {
@@ -36,12 +50,15 @@ int main () {
         if (!strcmp(option, "1")) {
             
             char pass[100];
-            printf("Enter the password (max 20 characters): ");
+            printf("Enter the password: ");
             scanf("%s", &pass);
             
-            if (strlen(pass) < 8) printf("Error: Password should be at least 8 characters long\n");
+            if (strlen(pass) < 8) {
+                printf("Error: Password should be at least 8 characters long\n");
+                continue;
+            }
             
-            
+            printf("Password Strength: %d\n", evaluateStrength(pass));
             
         } else if (!strcmp(option, "2")) {
             continue;
