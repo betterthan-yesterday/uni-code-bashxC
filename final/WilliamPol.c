08167c89c069d5bc79258b1ae39aa226dc758d0d
William Pol <103951515+betterthan-yesterday@users.noreply.github.com>
2023-11-13 20:43:06 -0500

    Completed assignment

--- final/WilliamPol.c
+++ final/WilliamPol.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 
+// Function Declaration
 int hasLowercase(char str[]) {
     for (int i = 0; i < strlen(str); i++) {
         if (islower(str[i]) && isalpha(str[i])) return 1;
@@ -38,8 +39,10 @@ int evaluateStrength(char str[]) {
 int main () {
     
     char option[1];
+    char* eval;
     
     do {
+        // Main Menu
         printf("\nPassword Resilience Analyzer\n");
         printf("1. Test a new password\n");
         printf("2. View strength of last tested password\n");
@@ -47,24 +50,47 @@ int main () {
         printf("Enter your choice: ");
         scanf("%s", &option);
         
-        if (!strcmp(option, "1")) {
+        // Menu navigation
+        if (!strcmp(option, "1")) { // Password testing
             
             char pass[100];
             printf("Enter the password: ");
             scanf("%s", &pass);
             
+            // Check for adequate length
             if (strlen(pass) < 8) {
                 printf("Error: Password should be at least 8 characters long\n");
                 continue;
             }
             
-            printf("Password Strength: %d\n", evaluateStrength(pass));
+            // Evaluate strength
+            switch (evaluateStrength(pass)) {
+                case 0: 
+                    eval = "Weak";
+                    break;
+                case 1: 
+                    eval = "Weak";
+                    break;
+                case 2: 
+                    eval = "Weak";
+                    break;
+                case 3: 
+                    eval = "Moderate";
+                    break;
+                case 4: 
+                    eval = "Moderate";
+                    break;
+                case 5: 
+                    eval = "Strong";
+                    break;
+            }
             
-        } else if (!strcmp(option, "2")) {
-            continue;
-        } else printf("Error: Invalid expression\n");
+            printf("Password Strength: %s\n", eval);
+            
+        } else if (!strcmp(option, "2")) printf("Last tested password strength: %s\n", eval); // See last password strength
+        else printf("Error: Invalid expression\n"); // Invalid expression
         
-    } while (strcmp(option, "3"));
+    } while (strcmp(option, "3")); // Check for exit option
     printf("Exiting... ");
 
     return 0;
